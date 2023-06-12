6abd4e2e3bf272a85ab0b5b354f55e0421f48c46
Affan <349980748@gapps.yrdsb.ca>
2023-06-12 16:34:43 -0400

    weee

--- TypingWorld.java
+++ TypingWorld.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.HashMap;
 /**
  * Write a description of class TypingWorld here.
  * 
@@ -13,14 +13,22 @@ public class TypingWorld extends World
      * Constructor for objects of class TypingWorld.
      * 
      */
+    private GreenfootImage[] images = new GreenfootImage[26];
+    private HashMap<String, GreenfootImage> characters = new HashMap<String, GreenfootImage>();
     public TypingWorld()
     {    
-        super(480, 320, 1, false);
+        super(700, 600, 1, false);
         GreenfootImage image = new GreenfootImage("EnterFileName.png");
-        image.scale(480, 320);
+        image.scale(700, 600);
         setBackground(image);
+        for(int i = 0; i < images.length; i++){
+            images[i] = new GreenfootImage("Character" + i + ".png");
+        }
     }
     public void act(){
-        
+        for(int i = 0; i < 26; i++){
+            characters.put(String.valueOf((char)(65 + i)), images[i]);
+        }
+        Greenfoot.getKey();
     }
 }
