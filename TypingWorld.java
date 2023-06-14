13bb146609667d9695c99862d8540ff8437458ae
Affan <349980748@gapps.yrdsb.ca>
2023-06-14 18:01:50 -0400

    backspace works and some typing world bugs fixed

--- TypingWorld.java
+++ TypingWorld.java
@@ -21,6 +21,9 @@ public class TypingWorld extends World
     private SimpleTimer timer = new SimpleTimer();
     private boolean newFile;
     private Alphabet[] alph = new Alphabet[7];
+    private int actCount = 0;
+    private int lastRunAct = actCount;
+    private boolean inRow = false;
     public TypingWorld(boolean newFile)
     {    
         super(700, 600, 1, false);
@@ -39,81 +42,16 @@ public class TypingWorld extends World
     public void act(){
         if(timer.millisElapsed() > 300){
         //String nonsense = Greenfoot.getKey();
-        if(spaceNumber == 0){
-            String key = Greenfoot.getKey();
-            if(key != null){
-                spaceNumber++;
+        if(Greenfoot.isKeyDown("BACKSPACE")){
+            if((lastRunAct + 15) < actCount && spaceNumber > 1){
+                lastRunAct = actCount;
+                spaceNumber--;
+                System.out.println(spaceNumber);
+                removeObject(alph[spaceNumber - 1]);
+                System.out.println("weeeee");
             }
         }
-        if(spaceNumber == 1){
-            String key = Greenfoot.getKey();
-            alph[0] = new Alphabet(characters.get(key));
-            addObject(alph[0], 294, 220);
-            if(key != null){
-                entry = entry + key;
-                spaceNumber++;
-            }
-        }
-        if(spaceNumber == 2){
-            String key = Greenfoot.getKey();
-            alph[1] = new Alphabet(characters.get(key));
-            addObject(alph[1], 317, 220);
-            if(key != null){
-                entry = entry + key;
-                spaceNumber++;
-            }
-        }
-        if(spaceNumber == 3){
-            String key = Greenfoot.getKey();
-            alph[2] = new Alphabet(characters.get(key));
-            addObject(alph[2], 340, 220);
-            if(key != null){
-                entry = entry + key;
-                spaceNumber++;
-            }
-        }
-        if(spaceNumber == 4){
-            String key = Greenfoot.getKey();
-            alph[3] = new Alphabet(characters.get(key));
-            addObject(alph[3], 363, 220);
-            if(key != null){
-                entry = entry + key;
-                spaceNumber++;
-            }
-        }
-        if(spaceNumber == 5){
-            String key = Greenfoot.getKey();
-            alph[4] = new Alphabet(characters.get(key));
-            addObject(alph[4], 386, 220);
-            if(key != null){
-                entry = entry + key;
-                spaceNumber++;
-            }
-        }
-        if(spaceNumber == 6){
-            String key = Greenfoot.getKey();
-            alph[5] = new Alphabet(characters.get(key));
-            addObject(alph[5], 409, 220);
-            if(key != null){
-                entry = entry + key;
-                spaceNumber++;
-            }
-        }
-        if(spaceNumber == 7){
-            String key = Greenfoot.getKey();
-            alph[6] = new Alphabet(characters.get(key));
-            addObject(alph[6], 432, 220);
-            if(key != null){
-                entry = entry + key;
-                spaceNumber++;
-            }
-        }
-        String key = Greenfoot.getKey();
-        if("BACKSPACE".equals(key)){
-            removeObject(alph[spaceNumber - 1]);
-            System.out.println("weeeee");
-        }
-        if(Greenfoot.isKeyDown("ENTER")){
+        else if(Greenfoot.isKeyDown("ENTER")){
             if(!newFile){
                 Data d = new Data(entry + ".csv");
                 GymWorld w = new GymWorld(d);
@@ -123,7 +61,76 @@ public class TypingWorld extends World
                 Greenfoot.setWorld(w);
             }
         }
-        //System.out.println(spaceNumber);
+        else if(spaceNumber == 0){
+            String key = Greenfoot.getKey();
+            if(key != null){
+                spaceNumber++;
+            }
+        }
+        else if(spaceNumber == 1){
+            String key = Greenfoot.getKey();
+            if(key != null && characters.get(key) != null){
+                alph[0] = new Alphabet(characters.get(key));
+                addObject(alph[0], 294, 220);
+                entry = entry + key;
+                spaceNumber++;
+            }
+        }
+        else if(spaceNumber == 2){
+            String key = Greenfoot.getKey();
+            if(key != null && characters.get(key) != null){
+                alph[1] = new Alphabet(characters.get(key));
+                addObject(alph[1], 317, 220);
+                entry = entry + key;
+                spaceNumber++;
+            }
+        }
+        else if(spaceNumber == 3){
+            String key = Greenfoot.getKey();
+            if(key != null && characters.get(key) != null){
+                alph[2] = new Alphabet(characters.get(key));
+                addObject(alph[2], 340, 220);
+                entry = entry + key;
+                spaceNumber++;
+            }
+        }
+        else if(spaceNumber == 4){
+            String key = Greenfoot.getKey();
+            if(key != null && characters.get(key) != null){
+                alph[3] = new Alphabet(characters.get(key));
+                addObject(alph[3], 363, 220);
+                entry = entry + key;
+                spaceNumber++;
+            }
+        }
+        else if(spaceNumber == 5){
+            String key = Greenfoot.getKey();
+            if(key != null && characters.get(key) != null){
+                alph[4] = new Alphabet(characters.get(key));
+                addObject(alph[4], 386, 220);
+                entry = entry + key;
+                spaceNumber++;
+            }
+        }
+        else if(spaceNumber == 6){
+            String key = Greenfoot.getKey();
+            if(key != null && characters.get(key) != null){
+                alph[5] = new Alphabet(characters.get(key));
+                addObject(alph[5], 409, 220);
+                entry = entry + key;
+                spaceNumber++;
+            }
+        }
+        else if(spaceNumber == 7){
+            String key = Greenfoot.getKey();
+            if(key != null && characters.get(key) != null){
+                alph[6] = new Alphabet(characters.get(key));
+                addObject(alph[6], 432, 220);
+                entry = entry + key;
+                spaceNumber++;
+            }
+        }
+        actCount++;
         }
     }
 }
