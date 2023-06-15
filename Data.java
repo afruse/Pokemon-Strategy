7d5e58d7f0c20f63f3a371207f718dec335e5ea5
gajans12345 <90165455+gajans12345@users.noreply.github.com>
2023-06-15 09:13:30 -0400

    Add files via upload

--- Data.java
+++ Data.java
@@ -13,6 +13,14 @@ import java.io.FileNotFoundException;
 import java.io.*;
 import java.util.*;
 
+/**
+ * A class of world that represents the world the player moves around in to challenge enemies
+ * 
+ * @author (Affan H and Gajan S) 
+ * @version 1.0
+ */
+
+
 public class Data {
     private ArrayList<String[]> values;
     private String currentWorldX;
@@ -23,6 +31,10 @@ public class Data {
     private String pokemon2;
     private String HP, lvl,XP,XP_needed, HP2, lvl2, XP2, XP_needed2;
     private ScanFile scan;
+    /**
+     * The constructer where it creates the pokemon and sets where it spawns in the grid
+     * @param fileName     Represents the x Index of the 2dArray of the grid
+     */
     public Data(String fileName) {
         scan = new ScanFile(fileName);
         this.values = scan.getInfo();
@@ -38,51 +50,75 @@ public class Data {
         this.XP2 = values.get(2)[1];
         this.XP_needed2 = values.get(2)[2];
     }
-
     public Data() {
     }
-
+    /**
+     * A method which updates the changes to the players data
+     * @param values    This represents all key player data, including the currentWorld, coordinates and the level
+     
+     */
     public void updateChanges(ArrayList<String []> values){
         this.values = values;
     }
-
+    /**
+     * A method which saves the changes from the player data and writes it into a file
+     * @param fileName    This represents the filename
+     * @param values      This represents the player data in the form of an arraylist
+     
+     */
     public void saveChanges(String fileName, ArrayList<String []> values){
+        
         this.values = values;
         ArrayList<String[]> waitList = values;
-        try{
-            FileWriter out = new FileWriter(fileName);
-            PrintWriter output = new PrintWriter(out);
-            for (int i = 0; i < waitList.size(); i++)
-            {
-                for (int j = 0; j < waitList.get(i).length; j++)
+            try{
+                FileWriter out = new FileWriter(fileName);
+                PrintWriter output = new PrintWriter(out);
+                for (int i = 0; i < waitList.size(); i++)
                 {
-                    output.print(waitList.get(i)[j] + ",");
+                    for (int j = 0; j < waitList.get(i).length; j++)
+                    {
+                        output.print(waitList.get(i)[j] + ",");
+                    }
+                    output.println();
                 }
-                output.println();
+                output.close();
             }
-            output.close();
-        }
-        catch(IOException e){
-            System.out.println("Error: " + e);
-        }
+            catch(IOException e){
+                System.out.println("Error: " + e);
+            }
     }
-
+    /**
+     * A method which gets current world x value representation
+     * @return int  This returns the integer representing a specific world x value representation
+     */
     public int getCurrentWorldX() {
         return Integer.parseInt(currentWorldX);
     }
-
+    /**
+     * A method which gets current y value of current world
+     * @return int  This returns the y value
+     */
     public int getCurrentWorldY() {
         return Integer.parseInt(currentWorldY);
     }
-
+    /**
+     * A method which gets current x 
+     * @return int  This returns the x coordinate
+     */
     public int getCoordinateX() {
         return Integer.parseInt(coordinateX);
     }
-
+    /**
+     * A method which gets current y 
+     * @return int  This returns the y coordinate
+     */
     public int getCoordinateY() {
         return Integer.parseInt(coordinateY);
     }
-
+    /**
+     * A method which determines if bos is beaten 
+     * @return boolean  This returns if boss is beaten
+     */
     public boolean getBeatBoss() {
         if(beatBoss.equals("true")){
             return true;
@@ -91,46 +127,40 @@ public class Data {
             return false;
         }
     }
-
+    /**
+     * A method which gets current HP
+     * @return int  This returns the HP value
+     */
     public int getHp() {
         return Integer.parseInt(HP);
     }
-
     public int getLvl()
     {
         return Integer.parseInt(lvl);
     }
-
     public String getPokemon1(){
         return pokemon1;
     }
-
     public String getPokemon2(){
         return pokemon2;
     }
-
     public int getXp() {
         return Integer.parseInt(XP);
     }
-
     public int getXpNeeded() {
         return Integer.parseInt(XP_needed);
     }
-
     public int getHp2()
     {
         return Integer.parseInt(HP2);
     }
-
     public int getLvl2(){
         return Integer.parseInt(lvl2);
     }
-
     public int getXp2()
     {
         return Integer.parseInt(XP2);
     }
-
     public int getXpNeeded2()
     {
         return Integer.parseInt(XP_needed2);
