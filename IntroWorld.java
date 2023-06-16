7771fdb2818d8547b6b8d6763ebe0dec7dcd7237
Daniel-Tann <dtan89609@gmail.com>
2023-06-15 23:39:15 -0400

    FINAL

--- IntroWorld.java
+++ IntroWorld.java
@@ -99,6 +99,7 @@ public class IntroWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(700, 600, 1, false); 
+        Greenfoot.setSpeed(50);
         backGroundAni = new GifIntroWorldBackground();
         addObject(backGroundAni, -400, -300);
         addObject(new GifOverlay(), 350, 300);
